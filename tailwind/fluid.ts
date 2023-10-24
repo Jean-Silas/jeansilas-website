d25836131da01eba4c79958f2f28db5b4b4b22c7
JeanSilasAtWiselich <jeansilas@wiselich.com>
2023-10-24 12:27:58 -0600

    scaffold mentoring page

--- tailwind/fluid.ts
+++ tailwind/fluid.ts
@@ -5,6 +5,9 @@ export default plugin(({ addUtilities, matchUtilities, theme }) => {
         '.text-5xl-fluid': {
             'font-size': 'clamp(1rem, 15vw, 3rem)',
         },
+        '.text-4xl-fluid': {
+            'font-size': 'clamp(1.5rem, 10vw, 3rem)',
+        },
         '.text-2xl-fluid': {
             'font-size': 'clamp(0.75rem, 8vw, 1.5rem)',
             'line-height': 'clamp(1.75rem, 8vw, 2rem)',
@@ -12,5 +15,34 @@ export default plugin(({ addUtilities, matchUtilities, theme }) => {
         '.text-xl-fluid': {
             'font-size': 'clamp(0.75rem, 8vw, 1.125rem)',
         },
+
     });
-}, {});
+    matchUtilities(
+        {
+            'text-fc': (value: string) => {
+                const parsed_value = value.split(' ');
+                return {
+                    fontSize: `clamp(${parsed_value[0]}, ${parsed_value[1]}, ${parsed_value[2]})`,
+                    // whiteSpace: 'nowrap',
+                }
+            }
+        },
+        { values: theme('containerSize') },
+    );
+}, {
+    theme: {
+        fluidSize: {
+            h1: '4',
+            h2: '3',
+            h3: '2',
+            h4: '1',
+        },
+        containerSize: {
+            sm: '1.25rem_3vw_3rem',
+            md: '1.25rem_3vw_3rem',
+            lg: '1.25rem_3vw_3rem',
+            xl: '1.25rem_3vw_3rem'
+        },
+        extend: {},
+    },
+});
