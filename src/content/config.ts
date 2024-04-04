a55f97487ffa487bce18d61f77130bf9e6c29efc
JeanSilasAtWiselich <jeansilas@wiselich.com>
2024-04-04 11:24:06 -0600

    Rought out archive section

--- src/content/config.ts
+++ src/content/config.ts
@@ -43,8 +43,64 @@ const serviceCollection = defineCollection({
     }),
 });
 
+const archiveBase = z.object({
+    title: z.string(),
+    subtitle: z.string().optional(),
+    role: z.enum(['root', 'section', 'page']),
+    kind: z.enum(['tutorial', 'how-to', 'explanation', 'reference']),
+    status: z.enum(['draft', 'published', 'deprecated']),
+    coverImage: z.string().optional(),
+
+    // TODO: Setup some sort of pre-commit hook to make sure these are updated
+    pubDate: z.date(),
+    editDate: z.date(),
+    
+    // To keep 001, 002, etc, out of the file names themselves.
+    sort: z.number().optional(),
+
+    // Basic namespacing setup for tags
+    tags: 
+        z.record(
+            z.string(), 
+            z.string().array()
+        )
+        .array()
+        .optional(),
+
+    // Hoistable term definitions
+    glossary: 
+        z.record(
+            z.string(),
+            z.object({
+                desc: z.string(),
+                tags: z.string().array().optional(),
+                local: z.boolean().optional()
+        }))
+        .array()
+        .optional(),
+
+    related: 
+        z.record(
+            z.string(),
+            z.union([
+                reference('blog'),
+                reference('knb'), 
+                reference('scratch'), 
+                reference('archive')
+            ]),
+        )
+        .array()
+        .optional(),
+})
+
+const archiveCollection = defineCollection({
+    type: 'content',
+    schema: archiveBase
+})
+
 export const collections = {
     blog: blogCollection,
     blocks: blockCollection,
     services: serviceCollection,
+    archive: archiveCollection
 };
--- src/utils/ContentMap.ts
+++ src/utils/ContentMap.ts
@@ -1,4 +1,5 @@
 import { getCollection } from 'astro:content';
+import type { CollectionEntry } from 'astro:content';
 
 const blogEntries = await getCollection('blog');
 
@@ -10,3 +11,27 @@ export const blogTitles = blogEntries
         // Do this better
         decorator: entry.data.status === 'draft' ? '[DRAFT] ' : '',
     }));
+
+
+export async function generateArchiveMap() {
+    let collectionEntries: CollectionEntry<'archive'>[] = await getCollection('archive');
+
+    const archiveMap: Map<string, {root: CollectionEntry<'archive'>, entries: CollectionEntry<'archive'>[]}> = new Map();
+
+    collectionEntries.forEach((entry) => {
+        const slug = entry.slug.split('/').slice(0, -1).join('/');
+        console.log(slug);
+        let mapEntry = archiveMap.get(slug) ?? {root: {}, entries: []};
+
+        if (entry.data.role === 'root') {
+            mapEntry.root = entry;
+        } else {
+            // @ts-ignore
+            mapEntry.entries.push(entry);
+        }
+        // @ts-ignore
+        archiveMap.set(slug, mapEntry);
+    });
+
+    return archiveMap
+}
