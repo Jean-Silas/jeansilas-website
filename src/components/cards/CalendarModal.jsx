03294374562a62cdbf700fdecdfcd4af8315a6f3
Jean-Silas <jeansilas@wiselich.com>
2024-07-24 08:57:46 -0600

    Update Calendar Link

--- src/components/cards/CalendarModal.jsx
+++ src/components/cards/CalendarModal.jsx
@@ -1,23 +1,29 @@
 import { getCalApi } from '@calcom/embed-react';
 import { useEffect } from 'react';
-export default function App() {
-    useEffect(() => {
-        (async function () {
-            const cal = await getCalApi();
-            cal('ui', {
-                styles: { branding: { brandColor: '#22242f' } },
-                hideEventTypeDetails: false,
-                layout: 'month_view',
-            });
-        })();
-    }, []);
-    return (
-        <button
-            className="m-4 rounded-xl bg-space-600 p-4 font-bold"
-            data-cal-link="jeansilas/intro-call"
-            data-cal-config='{"layout":"month_view"}'
-        >
-            Schedule an Intro Call
-        </button>
-    );
-}
+export default function MyApp() {
+  useEffect(()=>{
+    (async function () {
+      const cal = await getCalApi({"namespace":"intro-call"});
+      cal("ui", { 
+        "theme": "dark",
+        "cssVarsPerTheme": {
+            "dark": {
+                "cal-brand": "#22242f"
+            }
+        },
+        "hideEventTypeDetails":false,
+        "layout":"month_view"
+      });
+    })();
+  }, [])
+
+  return (
+    <button
+        className="m-4 rounded-xl bg-space-600 p-4 font-bold"
+        data-cal-namespace="intro-call"
+        data-cal-link="jean-silas/intro-call"
+        data-cal-config='{"layout":"month_view"}'
+    >
+        Schedule an Intro Call
+    </button>);
+};
