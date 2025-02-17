9c8dfa32f8cdaef25a154f177890acec0d51e6db
Jean-Silas <jeansilas@wiselich.com>
2025-02-17 12:00:42 -0700

    Convert astro config to typescript

---
+++ astro.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel';
+import mdx from '@astrojs/mdx';
+
+import react from '@astrojs/react';
+import remarkHint from 'remark-hint';
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [
+        tailwind(),
+        mdx({
+            remarkPlugins: [remarkHint],
+        }),
+        react(),
+    ],
+    output: 'server',
+    adapter: vercel(),
+});
